07b42ca2e5607c9935eb6f352de7b446ba6dd6c6
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2026-04-01 12:26:24 -0500

    worked on some stuff during lunch

---
+++ a_tour_of_Go/newTest.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func concat(name1, name2 string) string {
+	return name1 + " " + name2
+}
+
+func addition(x, y int) int {
+	return x + y
+}
+
+func convert(num1 string) int {
+	new_num, err := strconv.Atoi(num1)
+
+	if err != nil {
+		fmt.Println("Conversion failed.")
+	} else {
+		return new_num
+	}
+}
+
+func main() {
+	word := "Pleasant"
+	wordy := "Day"
+	x := 17
+	y := 18
+	z := "one"
+	fmt.Printf("Well, hello there! %v isn't it?\n", concat(word, wordy))
+	fmt.Printf("The sum of your two numbers is: %v\n", addition(x, y))
+	fmt.Printf("The numerical representation of 'One' is: %v\n", convert(z))
+}
