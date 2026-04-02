33f847820a237f4eb2659dbef9aae0e26318c134
chris jones <134346433+Sparky-turned-Coder@users.noreply.github.com>
2026-04-01 21:04:19 -0500

    playing with functions and stringtoint conversions

--- a_tour_of_Go/newTest.go
+++ a_tour_of_Go/newTest.go
@@ -13,23 +13,25 @@ func addition(x, y int) int {
 	return x + y
 }
 
-func convert(num1 string) int {
-	new_num, err := strconv.Atoi(num1)
-
-	if err != nil {
+func convert(myString string) int {
+	x, err := strconv.Atoi(myString)
+	if err != nil || x < 1 {
 		fmt.Println("Conversion failed.")
 	} else {
-		return new_num
+		return x
 	}
+	return x
 }
 
 func main() {
 	word := "Pleasant"
 	wordy := "Day"
-	x := 17
-	y := 18
-	z := "one"
+	num1 := 31
+	num2 := 21
+	x := "17"
+	y := "18"
+	z := "155"
 	fmt.Printf("Well, hello there! %v isn't it?\n", concat(word, wordy))
-	fmt.Printf("The sum of your two numbers is: %v\n", addition(x, y))
-	fmt.Printf("The numerical representation of 'One' is: %v\n", convert(z))
+	fmt.Printf("The sum of your two numbers is: %v\n", addition(num1, num2))
+	fmt.Printf("The numerical representation of the strings '%v', '%v', and '%v' is %v, %v, %v, respectively.\n", x, y, z, convert(x), convert(y), convert(z))
 }
