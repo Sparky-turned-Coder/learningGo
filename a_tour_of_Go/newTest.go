e9cee8b99c758af9f06d247e5a7fa804495ca02f
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2026-04-03 13:21:00 -0500

    updated newTest.go

--- a_tour_of_Go/newTest.go
+++ a_tour_of_Go/newTest.go
@@ -17,28 +17,13 @@ func convert(myString string) int {
 	x, err := strconv.Atoi(myString)
 	if err != nil || x < 1 {
 		fmt.Println("Conversion failed.")
-<<<<<<< HEAD
-	}
-	return new_num
-=======
-	} else {
-		return x
 	}
 	return x
->>>>>>> a0ca85a5f1be567a013981082f5187cf1fa81e79
 }
 
 func main() {
 	word := "Pleasant"
 	wordy := "Day"
-<<<<<<< HEAD
-	x := 17
-	y := 18
-	z := "1"
-	fmt.Printf("Well, hello there! %v isn't it?\n", concat(word, wordy))
-	fmt.Printf("The sum of your two numbers is: %v\n", addition(x, y))
-	fmt.Printf("The numerical representation of string '1' is: %v\n", convert(z))
-=======
 	num1 := 31
 	num2 := 21
 	x := "17"
@@ -47,5 +32,4 @@ func main() {
 	fmt.Printf("Well, hello there! %v isn't it?\n", concat(word, wordy))
 	fmt.Printf("The sum of your two numbers is: %v\n", addition(num1, num2))
 	fmt.Printf("The numerical representation of the strings '%v', '%v', and '%v' is %v, %v, %v, respectively.\n", x, y, z, convert(x), convert(y), convert(z))
->>>>>>> a0ca85a5f1be567a013981082f5187cf1fa81e79
 }
