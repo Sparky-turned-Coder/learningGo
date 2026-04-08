ebc8a7e5a927ff89a579c87e4d571c8d3194f64b
chris jones <134346433+Sparky-turned-Coder@users.noreply.github.com>
2026-04-07 20:00:24 -0500

    hidden power base power calc a GO

--- pokemon/powerHiddenPower.go
+++ pokemon/powerHiddenPower.go
@@ -1,22 +1,9 @@
 package main
 
-import "fmt"
-
-/*
-Step 1: Declare variables
-		HP, Attack, Defense, Speed, Special Attack, Special Defense
-		U	  V		   W	   X		  Y				 Z
-Step 2: Check (if remainder of Stat / 4 equals 2 or 3):
-
-		For each stat (Hp, Atk, Def, Speed, Sp.Atk, Sp.Def), check if the IV value divided by 4 has a remainder of 2 or 3 (modulo). If yes, the variable is used; otherwise, it is 0.
-		HP - 1		Speed - 8
-		ATK - 2		Sp.Atk - 16
-		DEF - 4		Sp.Def - 32
-		So, when the stat is divided by 4, if remainder == 2 || 3, use value above; if remainder != 2 || 3, use 0.  (Modulo operand)
-
-	Formula:
-		[(U + V + W + X + Y + Z) * 40/63 + 30]
-*/
+import (
+	"fmt"
+	"math"
+)
 
 func statPrompt(prompt string) int {
 	var stat int
@@ -39,17 +26,19 @@ func powerFormula(u, v, w, x, y, z int) float64 {
 }
 
 func main() {
+	var pokeName string
+
+	fmt.Printf("What pokemon are we checking on today? ")
+	fmt.Scanln(&pokeName)
+
 	HP := statPrompt("Enter the IV value of your HP: ")
 	Atk := statPrompt("Enter the IV value of your Attack: ")
 	Def := statPrompt("Enter the IV value of your Defense: ")
 	Speed := statPrompt("Enter the IV value of your Speed: ")
 	SpAtk := statPrompt("Enter the IV value of your Special Attack: ")
 	SpDef := statPrompt("Enter the IV value of your Special Defense: ")
-	// var confirm string
 
 	fmt.Printf("You entered the following values for your IV's. HP: %d, Attack: %d, Defense: %d, Speed: %d, Special Attack: %d, Special Defense: %d\n", HP, Atk, Def, Speed, SpAtk, SpDef)
-	// fmt.Printf("Do these look correct? (y/n)")
-	// fmt.Scanln(&confirm)
 
 	u := remainderCheck(HP, 1)
 	v := remainderCheck(Atk, 2)
@@ -58,5 +47,39 @@ func main() {
 	y := remainderCheck(SpAtk, 16)
 	z := remainderCheck(SpDef, 32)
 
-	fmt.Printf("The base power of your Hidden Power ability is: %.2f\n", powerFormula(u, v, w, x, y, z))
+	fmt.Printf("The base power of your %s's Hidden Power ability is: %.2f\n", pokeName, math.Floor(powerFormula(u, v, w, x, y, z)))
 }
+
+/* Our Guinea Pig:
+
+Crobat - IVs
+	HP - 20
+	Atk - 23
+	Def - 17
+	Sp. Atk - 29
+	Sp. Def - 2
+	Speed - 12
+
+Result:
+	Base Power - 51
+	Type - Bug  (I looked this up online. Let's see if OUR type calculator comes up with the same.)
+
+*/
+
+/*
+Steps we went through when figuring this pokemon calculator out:
+
+Step 1: Declare variables
+		HP, Attack, Defense, Speed, Special Attack, Special Defense
+		U	  V		   W	   X		  Y				 Z
+Step 2: Check (if remainder of Stat / 4 equals 2 or 3):
+
+		For each stat (Hp, Atk, Def, Speed, Sp.Atk, Sp.Def), check if the IV value divided by 4 has a remainder of 2 or 3 (modulo). If yes, the variable is used; otherwise, it is 0.
+		HP - 1		Speed - 8
+		ATK - 2		Sp.Atk - 16
+		DEF - 4		Sp.Def - 32
+		So, when the stat is divided by 4, if remainder == 2 || 3, use value above; if remainder != 2 || 3, use 0.  (Modulo operand)
+
+	Formula:
+		[(U + V + W + X + Y + Z) * 40/63 + 30]
+*/
